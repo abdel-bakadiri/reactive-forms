ac8ffda443c0a12f549c61a00250cdeea3fd0c2c
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-05-21 15:24:36 +0200

    create template diven forms

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
-import { Customer } from './customer';
+import { Customer } from "./customer";
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  selector: "app-customer",
+  templateUrl: "./customer.component.html",
+  styleUrls: ["./customer.component.css"],
 })
 export class CustomerComponent implements OnInit {
   customer = new Customer();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   save(customerForm: NgForm) {
     console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+    console.log("Saved: " + JSON.stringify(customerForm.value));
   }
 }
